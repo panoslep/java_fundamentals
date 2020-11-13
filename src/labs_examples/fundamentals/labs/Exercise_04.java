728e00e0abc6e8ab77a9290cc5dffcf3f7af426c
Panos Lepipas <69046291+panoslep@users.noreply.github.com>
2020-11-13 18:38:43 +0000

    first 3 exercises

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -11,9 +11,25 @@ package labs_examples.fundamentals.labs;
 public class Exercise_04 {
 
     public static void main(String[] args) {
+        byte myByte = 3;         // byte
+        short myShortNum = 59;   //Short
+        int i = 1;               // Integer (whole number)
+        long myLondNum = 3165484L;   // Long
+        float myFloatNum = 5.99f;    // Floating point number
+        double myDoubleNum = 3165484.4654641d; // Double
+        char myLetter = 'D';         // Character
+        boolean myBool = true;       // Boolean
+        String myText = "Hello";     // String
 
-        int i = 1;
+        System.out.println("byte myByte is: " + myByte);
+        System.out.println("short myShortNum is: " + myShortNum);
         System.out.println("int i is: " + i);
+        System.out.println("long myLondNum is: " + myLondNum);
+        System.out.println("float myFloatNum is: " + myFloatNum);
+        System.out.println("double myDoubleNum is: " + myDoubleNum);
+        System.out.println("char myLetter is: " + myLetter);
+        System.out.println("boolean myBool is: " + myBool);
+        System.out.println("String myText is: " + myText);
 
         // write your code below
 
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,14 +14,40 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int lengthOfstr= str.length();
+        System.out.println(lengthOfstr);
 
-        String str2 = "hello";
+        String str2 = "HELLO!";
         // please initialize a boolean variable and test whether str is equal to str2
+        boolean isEqual;
+        isEqual = str.equalsIgnoreCase(str2);
+        System.out.println(isEqual);
 
         // please concatenate str & str2 and set the result to a new String variable below
+        String conc = str+" "+str2;
+        System.out.println(conc);
+
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+
+        //replace()
+        str = str.replace("!","?");
+        System.out.println(str);
+
+        //substring()
+        String sub = str.substring(2,4);
+        System.out.println(sub);
+
+
+        //contains()
+        boolean cont = str.contains("he");
+        System.out.println(cont);
+
+        //indexOf()
+        int index = str.indexOf("l");
+        System.out.println(index);
+
 
     }
 
