cb3609e447e2daa3bd5cb997c4926c928d90f8e7
Panos Lepipas <69046291+panoslep@users.noreply.github.com>
2020-11-16 17:54:17 +0000

    datatypes_operators, solution to ex 7

--- src/labs_examples/datatypes_operators/labs/Exercise_07.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_07.java
@@ -22,8 +22,19 @@ public class Exercise_07 {
         System.out.print("Enter a number in days between 1 and 1,000,000: ");
         // assign input to variable as int
         int days = scanner.nextInt();
+        if (days<1)
+        {
+            System.out.println("You are out of range. I give you 1 day");
+            days =1;
+        }
+        if (days>1000000)
+        {
+            System.out.println("It can't take that long. I'll give it a million days");
+            days = 1000000;
+        }
 
         // write completed code here
-
+        int seconds = (days*24*60*60);
+        System.out.println(days+" days are "+seconds+" seconds");
     }
 }
