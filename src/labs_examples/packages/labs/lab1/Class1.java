fda45149bee1379f1f0800432e64c2ddd67c368a
Panos Lepipas <69046291+panoslep@users.noreply.github.com>
2020-12-18 18:14:01 +0000

    udpates to blackjack

--- src/labs_examples/packages/labs/lab1/Class1.java
+++ src/labs_examples/packages/labs/lab1/Class1.java
@@ -1,2 +1,32 @@
-package labs_examples.packages.labs.lab1;public class Class1 {
+package labs_examples.packages.labs.lab1;
+
+import labs_examples.packages.labs.lab2.Class2;
+
+
+public class Class1 {
+    public static void main(String[] args) {
+
+        Class1 object1 = new Class1();
+        object1.protectedMethod();
+        object1.defaultMethod();
+        Class2 object2 = new Class2();
+        object2.pubicMethod();
+
+        System.out.println("Attempt to access protected class:");
+        try {
+   //        object2.protectedMehod();
+        }
+        catch(Exception e) {
+            System.out.println("The method was not accessible");
+        }
+    }
+    protected void protectedMethod(){
+        System.out.println("This is a protected method of lab1.Class1");
+    }
+
+    void defaultMethod(){
+        System.out.println("This is a default method of labs.Class1");
+    }
+
 }
+
--- src/labs_examples/packages/labs/lab2/Class2.java
+++ src/labs_examples/packages/labs/lab2/Class2.java
@@ -1,2 +1,16 @@
-package labs_examples.packages.labs.lab2;public class Class2 {
+package labs_examples.packages.labs.lab2;
+
+public class Class2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    protected void protectedMethod(){
+        System.out.println("This is a protected method of lab2.Class2");
+    }
+
+    public void pubicMethod(){
+        System.out.println("This is a public method of lab2.Class2");
+    }
 }
