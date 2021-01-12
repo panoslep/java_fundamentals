8c0e352f6e5935d8610365eaa569adc1d7ae00f7
Panos Lepipas <69046291+panoslep@users.noreply.github.com>
2021-01-12 16:14:41 +0000

    changes to exception handling and IO

--- src/labs_examples/input_output/labs/Exercise_02.java
+++ src/labs_examples/input_output/labs/Exercise_02.java
@@ -12,3 +12,33 @@ package labs_examples.input_output.labs;
  *
  */
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+class EncryptionExample {
+    public static void main(String[] argv) throws Exception {
+
+        String filePathIn = "C:\\Users\\Matlab\\Documents\\CodingNomads\\labs\\online-java-fundamentals\\src\\labs_examples\\input_output\\labs\\ex2_original";
+        String filePathOut = "C:\\Users\\Matlab\\Documents\\CodingNomads\\labs\\online-java-fundamentals\\src\\labs_examples\\input_output\\labs\\ex2_encrypted";
+
+        BufferedReader br = new BufferedReader(new FileReader(filePathIn));
+        BufferedWriter bw = new BufferedWriter(new FileWriter(filePathOut));
+        int i;
+        do {
+            i = br.read();
+            if (i != -1) {
+                char j 
+                if (Character.isLowerCase((char) i))
+                    bw.write(((char) i));
+                else if (Character.isUpperCase((char) i))
+                    bw.write(((char) i));
+                else
+                    bw.write((char) i);
+            }
+        } while (i != -1);
+        br.close();
+        bw.close();
+    }
+}
