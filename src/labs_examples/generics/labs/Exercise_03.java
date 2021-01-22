134882cef9295e71bc4bd2ec2299b9c4cf6eff65
Panos Lepipas <69046291+panoslep@users.noreply.github.com>
2021-01-22 16:46:26 +0000

    some edits

--- src/labs_examples/generics/labs/Exercise_03.java
+++ src/labs_examples/generics/labs/Exercise_03.java
@@ -1,7 +1,5 @@
 package labs_examples.generics.labs;
 
-import javafx.scene.chart.ScatterChart;
-
 import java.util.*;
 
 /**
@@ -53,6 +51,102 @@ public class Exercise_03 {
         return palElements;
     }
 
+    public static <E> E[] reversePos(E[] array, int pos1, int pos2){
+        E temp = array[pos1];
+
+        try {
+            array[pos1] = array[pos2];
+            array[pos2] = temp;
+        }catch (Exception exp){
+            System.out.println("Something went wrong.");
+        }
+        return array;
+    }
+
+    public static <T extends Object & Comparable> void largestElement(ArrayList<T> list, int begin, int end) {
+
+        ArrayList<T> rangedList = new ArrayList<T>();
+        for (int i = begin; i <= end; i++){
+            rangedList.add(list.get(i));
+        }
+        System.out.println("The elements within range limits are: ");
+        for (T x : rangedList) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+        System.out.println("The largest element is : " +
+                Collections.max(list));
+    }
+
+//    public static <E> E largestElement2(ArrayList<E> list, int begin, int end) {
+//        E tempElement = list.get(0);
+//        int index = 0;
+//        try {
+//            if (tempElement instanceof Number) {
+//                double max = (double) list.get(begin); //Double.NEGATIVE_INFINITY;
+//                double max = 0;
+//                for (int i = begin; i <= end; i++) {
+//                    if ((double) list.get(i) > max) {
+//                        max = (double) list.get(i);
+//                        index = i;
+//                    }
+//                }
+//            } else if (tempElement instanceof String) {
+//                int max = 0;
+//                for (int i = begin; i <= end; i++) {
+//                    int len = list.get(i).toString().length();
+//                    if (len > max) {
+//                        max = len;
+//                        index = i;
+//                    }
+//                }
+//            } else {
+//                System.out.println("your list consists of non-comparable elements");
+//                return null;
+//            }
+//        } catch (Exception exp2) {
+//            System.out.println("Something went wrong.");
+//            return null;
+//        }
+//        return list.get(index);
+//    }
+
+
+//    public static <E extends Number> E largestElement3(ArrayList<E> list, int begin, int end) {
+//        int index = 0;
+//        try {
+//            double max = (double) list.get(begin); //Double.NEGATIVE_INFINITY;
+//            for (int i = begin; i <= end; i++) {
+//                if ((double) list.get(i) > max) {
+//                    max = (double) list.get(i);
+//                    index = i;
+//                }
+//            }
+//        } catch (Exception exp2) {
+//            System.out.println("Something went wrong.");
+//            return null;
+//        }
+//        return list.get(index);
+//    }
+
+    public static String largestElement3(ArrayList<String> list, int begin, int end) {
+        int index = 0;
+        try {
+            int max = 0;
+            for (int i = begin; i <= end; i++) {
+                int len = list.get(i).toString().length();
+                if (len > max) {
+                    max = len;
+                    index = i;
+                }
+            }
+        } catch (Exception exp3) {
+            System.out.println("Something went wrong.");
+            return null;
+        }
+        return list.get(index);
+    }
+
 
 
     public static void main(String args[]) {
@@ -60,10 +154,34 @@ public class Exercise_03 {
         Integer arg1 = 5;
         Float arg2 = 5.5f;
         List<String> listString = Arrays.asList("kayak", "panos", "ryan", "AManAPLanACanalPanama");
+        Integer[] array = { 1, 2, 3, 4, 5 };
+
+        ArrayList<String> arrayListString =
+                new ArrayList<>(Arrays.asList("a", "long", "word", "is", "strawberry", "."));
+
+        ArrayList<Integer> arrayListInt =
+                new ArrayList<>(Arrays.asList(10, 20, 50, 4668, 34, 453));
 
         System.out.println("\n" + arg1 + " + " + arg2 + " = " + doubleSum(arg1, arg2));
         System.out.println();
         int elements = palindromes(listString);
         System.out.println(elements + " elements in the list are palindromes");
+
+        System.out.println("Array before exchange: ");
+        for (int x : array) {System.out.print(x);}
+        System.out.println();
+        System.out.println("Array after exchange: ");
+        for (int x : reversePos(array, 2, 4)) {
+            System.out.print(x);
+        }
+
+        System.out.println();
+        largestElement(arrayListString, 2, 4);
+        System.out.println();
+        largestElement(arrayListInt, 0, 4);
+
     }
 }
+
+
+
