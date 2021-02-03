100c6632880fd7a92cc30384c5df22dbd0d76783
Panos Lepipas <69046291+panoslep@users.noreply.github.com>
2021-02-03 22:11:34 +0000

    pushing edits on exercise_03

--- src/labs_examples/lambdas/labs/Exercise_03.java
+++ src/labs_examples/lambdas/labs/Exercise_03.java
@@ -1,5 +1,7 @@
 package labs_examples.lambdas.labs;
 
+import java.util.function.BiFunction;
+
 /**
  * Lambdas Exercise 3:
  *
@@ -8,3 +10,21 @@ package labs_examples.lambdas.labs;
  *      3) Demonstrate the use of a constructor reference
  *
  */
+
+public class Exercise_03 {
+    public static void main(String[] args) {
+
+        // call a static method
+        BiFunction<String, String, String> connect = Exercise_03 :: connectWords;
+        String myString = connect.apply("the", "end");
+        System.out.println(myString);
+
+        
+    }
+
+    public static String connectWords(String s1, String s2) {
+        s1 = s1.substring(0, 1).toUpperCase() + s1.substring(1);
+        String string = s1 + " " + s2 + ".";
+        return string;
+    }
+}
