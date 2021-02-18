1b366ddcdd177b9f6c4e84f7ca22ab1e94ae500a
Panos Lepipas <69046291+panoslep@users.noreply.github.com>
2021-02-18 17:00:43 +0000

    started new package for snr input program

---
+++ src/SNR_Input/SNR_Input.java
@@ -0,0 +1,14 @@
+package SNR_Input;
+
+/**
+ * Created by Panos Lepipas
+ *
+ *  A program to read .csv and .xlsx files from given folder to extract and correlate data.
+ *
+ *
+ */
+
+
+public class SNR_Input {
+    
+}
